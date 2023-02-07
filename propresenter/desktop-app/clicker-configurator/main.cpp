8b151d5ffcb0f939cf31dd0091cc3a79e48465b3
SevRyb <rybka.severus@gmail.com>
2023-02-07 23:49:29 +0200

    Working on ui

--- propresenter/desktop-app/clicker-configurator/main.cpp
+++ propresenter/desktop-app/clicker-configurator/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QFile style_file("stylesheet.qss");
+    QFile style_file("../stylesheet.qss");
     style_file.open(QFile::ReadOnly);
     QString style_sheet = QLatin1String(style_file.readAll());
     app.setStyleSheet(style_sheet);
